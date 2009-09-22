85a5164ddae80c6d89e8e1312063f0c91cf00235
Rob McGuire-Dale <rob@osuosl.org>
2009-09-22 14:38:25 -0700

    settings.py files were just redundant copies of models.py. Application didn't seem to need them, so I deleted them. If they are found to be needed in the future, we need only to create symbolic links named settings.py to the models.py files.

--- screen_fooscreen/settings.py
+++
@@ -1,8 +0,0 @@
-import dbsettings
-
-""" ================================================================
-# Settings for fooscreen
-================================================================ """
-class FooScreenSettings(dbsettings.Group):
-    TEXT     = dbsettings.StringValue('box test', 'Text that will be displayed in the fooscreen box', default='This box was drawn by fooscreen')
-foo_settings = FooScreenSettings('Foo Screen Settings')
