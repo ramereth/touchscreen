046cd49e9e9b70ea041d1a8c52908cbb2b619b91
Rob McGuire-Dale <rob@osuosl.org>
2009-09-23 16:58:29 -0700

    Removed models from the osl_about screen because it was not needed.

--- screen_osl_about/models.py
+++ screen_osl_about/models.py
@@ -1,21 +1,3 @@
 from django.db import models
-import dbsettings
 
-""" ================================================================
-# Settings for screen_osl_about
-================================================================ """
-class screen_osl_aboutSettings( dbsettings.Group ):
-    
-    screen_border = dbsettings.IntegerValue(
-            'Screen Border', 
-            'The space between the edges of the screen and the tiles', 
-            default=10
-    )
-    
-    tile_padding = dbsettings.IntegerValue(
-            'Tile Padding', 
-            'The space between the tiles', 
-            default=10
-    )
-
-screen_osl_about_settings = screen_osl_aboutSettings('screen_osl_about Settings')
+# Create your models here.
