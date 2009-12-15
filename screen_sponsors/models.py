4dbdf9c3305e26a77518e0b712921118339b8936
Rob McGuire-Dale <rob@osuosl.org>
2009-12-14 19:16:03 -0800

    Tiles in sponsors screen position and size dynamically without centering. Committing before working on centering.

--- screen_sponsors/models.py
+++ screen_sponsors/models.py
@@ -8,32 +8,37 @@ class screen_sponsorsSettings( dbsettings.Group ):
     
     sponsors_rss_URL = dbsettings.StringValue(
         'OSL Sponsors RSS Feed URL', 
-        'URL of the RSS feed of the OSL sponsors', 
+        'URL of the RSS feed of the OSL sponsors.', 
         default='http://osuosl.org/members/rss.xml'
     )
     
     sponsors_rss_filename = dbsettings.StringValue(
         'OSL Sponsors Downloaded RSS File Name', 
-        'File name of the downloaded sponsors RSS XML file', 
+        'File name of the downloaded sponsors RSS XML file.', 
         default='sponsors.xml'
     )
     
     friends_rss_URL = dbsettings.StringValue(
         'OSL Friends Page URL', 
-        'URL of the page listing the friends of the OSL', 
+        'URL of the page listing the friends of the OSL.', 
         default='http://osuosl.org/friends/members'
     )
 
     friends_rss_filename = dbsettings.StringValue(
         'OSL Friends Downloaded RSS XML File Name', 
-        'File name of the downloaded friends RSS XML file', 
+        'File name of the downloaded friends RSS XML file.', 
         default='sponsors.xml'
     )
 
-    screen_max_width = dbsettings.IntegerValue(
-        'Maximum Screen Width',
-        'Maximum width of this screen',
-        default=760)
+    friends_width = dbsettings.IntegerValue(
+        'Friends Tile Width',
+        'The friends tile width. Will NOT dynamically resize with screen.',
+        default=150)
+        
+    sponsors_max_width = dbsettings.IntegerValue(
+        'Sponsors Tile Maximum Width',
+        'The sponsors tile maximum width. Will dynamically resize if the width of the sponsors and friends tiles exceeds that of the browser window.',
+        default=750)
 
 screen_sponsors_settings = screen_sponsorsSettings('screen_sponsors Settings')
 
