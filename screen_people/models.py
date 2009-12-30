f4f5c055620a1024e2f18b595e80190a0395449b
Rob McGuire-Dale <rob@osuosl.org>
2009-12-29 22:47:40 -0800

    Horizontal scroller set up with custom autoscroller working.

--- screen_people/models.py
+++ screen_people/models.py
@@ -12,5 +12,23 @@ class screen_peopleSettings( dbsettings.Group ):
             default='http://osuosl.org/about/people'
     )
 
+    thumbnail_height = dbsettings.IntegerValue(
+            'Thumbnail Image Height', 
+            'The height of the thumbnail images', 
+            default=150
+    )
+    
+    scroll_interval = dbsettings.IntegerValue(
+        'Scroll Interval',
+        'Time between scrolling (in milliseconds). Must be at least 50 more than scroll speed.',
+        default=3000
+    )
+    
+    scroll_speed = dbsettings.IntegerValue(
+        'Scroll Speed',
+        'Time it takes to scroll a single item (in milliseconds). Must be at least 50 less than scroll interval.',
+        default=1000
+    )
+    
 screen_people_settings = screen_peopleSettings('screen_people Settings')
 
