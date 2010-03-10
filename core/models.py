652f4e85a5f75595714706cf1065161bff0d163c
Peter Krenesky <peter@osuosl.org>
2010-03-10 09:40:04 -0800

    removing old code that was commented out

--- core/models.py
+++ core/models.py
@@ -1,44 +1,13 @@
 from django import forms
 from muddle.plugins.plugin import Plugin
 
-'''
-class Plugin(models.Model):
-    """
-    Base model for all plugins.
-    """
-    hash        = models.CharField(unique=True, max_length=255)
-    enabled     = models.BooleanField(default=True)
-    name        = None
-    app         = None
-    description = None
-    settings    = None
 
-    class Meta:
-        abstract = True
-
-    def generate_hash(self):
-        # self.hash = '%s_%s' % (self.app, self.name) <-- Original hash
-        self.hash = '%s' % self.app.replace('.','_') # sanitize name
-
-        
-
-    def validate(self):
-        if not name:
-            raise 'Plugin name cannot be null'
-
-        if not description:
-            raise 'Plugin description cannot be null'
-
-        return True
-'''
 class TouchscreenPlugin(Plugin):
     pass
 
 
 class ScreenGeneralSettings(forms.Form):
     duration    = forms.IntegerField(initial=10000, required=False)
-    #hide        = forms.CharField(max_length='30', initial="slide")
-    #show        = forms.CharField(max_length='30', initial="slide")
     slideshow   = forms.BooleanField(initial=True)
 
 
@@ -49,7 +18,6 @@ class Screen(TouchscreenPlugin):
     """
     target = 'ScreenManager'
     _target = 'ScreenManager'
-
     config_form = ScreenGeneralSettings
 
     # fields that are not parameters so they do not need to be changed on the fly
