eb0cf8f09acfef0791f2508822c85eecc024a69f
Peter Krenesky <peter@osuosl.org>
2010-03-10 08:42:51 -0800

    Sponsors screen - converted to muddle plugin

--- screens/sponsors/plugins.py
+++ screens/sponsors/plugins.py
@@ -1,21 +1,16 @@
-from core.models import *
-from models import sponsors_settings
+from core.models import Screen, ScreenGeneralSettings
+from django import forms
 
 """
 create instance of screens this plugin provides. The parameters are as follows:
 
 	Required:
 	- the name of the screen's html file (i.e. 'screen.html')
-	- the name of the screen (i.e. 'screen')
+	- the description of the screen (i.e. 'screen')
 
 	Optional:
 	- hide='<transition name>'	i.e. hide='fade', or hide='slide')
 	- show='<transition name>'	i.e. show='fade', or show='slide')
-	- duration='<time in ms>'	How long screen will be displayed. I.e. 
-								duration='3000' means to display the screen for 
-								3 seconds.
-	- slideshow='<true or false>'	If the screen is to be included in the 
-									slideshow
 	- js_init='<js function>'	a javascript function to call when the screen is
 								initialized.
 	- js_start='<js function>'	a javascript function to call when showing the 
@@ -25,19 +20,59 @@ create instance of screens this plugin provides. The parameters are as follows:
 
 """
 
-sponsors = Screen(
-    'sponsors.html',
-    'sponsors',
+class OSLSponsorsSettings(forms.Form):
+    sponsors_URL = forms.CharField(
+        label='OSL Sponsors RSS Feed URL',
+        help_text='URL of the RSS feed of the OSL sponsors.',
+        initial='http://osuosl.org/members/rss.xml',
+        max_length=128
+    )
+    
+    friends_URL = forms.CharField(
+        label='OSL Friends Page URL',
+        help_text='URL of the page listing the friends of the OSL.',
+        initial='http://osuosl.org/friends/members',
+        max_length=128
+    )
+
+    friends_width = forms.IntegerField(
+        label='Friends Tile Width',
+        help_text='The friends tile width. Will NOT dynamically resize with screen.',
+        initial=150)
+        
+    sponsors_max_width = forms.IntegerField(
+        label='Sponsors Tile Maximum Width',
+        help_text='The sponsors tile maximum width. Will dynamically resize if the width of the sponsors and friends tiles exceeds that of the browser window.',
+        initial=750)
+        
+    friends_scroll_interval = forms.IntegerField(
+        label='Friends Scroll Interval',
+        help_text='Time between scrolling of a friend (in milliseconds). Must be at least 50 greater than scroll speed.',
+        initial=3000)
+    
+    friends_scroll_speed = forms.IntegerField(
+        label='Friends Scroll Speed',
+        help_text='Time it takes to scroll a single item (in milliseconds). Must be at least 50 less than scroll interval.',
+        initial=1000)
+        
+    sponsors_scroll_interval = forms.IntegerField(
+        label='Sponsors Scroll Interval',
+        help_text='Time between scrolling of a sponsor (in milliseconds). Must be at least 50 greater than scroll speed.',
+        initial=10000)
+        
+    sponsors_scroll_speed = forms.IntegerField(
+        label='Sponsors Scroll Speed',
+        help_text='Time it takes to scroll a single item (in milliseconds). Must be at least 50 less than scroll interval.',
+        initial=2000)
+
+
+class OSL_Donors(Screen):
+    template = 'sponsors.html'
+    description = 'Open Source Lab sponsors'
 
     #optional params
-    duration=300000,
-    settings=sponsors_settings,
-    #hide='fade', 
-    #show='fade',
-    slideshow=True,
-    js_init ='init',
-    js_start='start',
-    js_stop ='stop',
+    config_form = (OSLSponsorsSettings, ScreenGeneralSettings)    
+    js_init ='init'
+    js_start='start'
+    js_stop ='stop'
     js_onWinResize = 'onScreenResize'
-)
-
