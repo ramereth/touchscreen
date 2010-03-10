0c9096328cd197a473194b97b5d09ad221b85018
Peter Krenesky <peter@osuosl.org>
2010-03-10 10:23:19 -0800

    adding admin, sqlite, authentication

--- urls.py
+++ urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import *
 import settings
-
+from django.contrib.auth.views import login, logout
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 
 urlpatterns = patterns('',
@@ -16,8 +16,11 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    #(r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', admin.site.root),
     
+    #authentication
+    (r'^accounts/login/$', login),
+    (r'^accounts/logout/$', logout, {'next_page':'/plugins'}),
 
     # Enable the touchscreen core
     (r'^/*', include('core.urls')),
