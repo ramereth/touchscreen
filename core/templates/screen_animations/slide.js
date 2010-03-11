0a5ddd9914ab49b114e1028e2f74048eb99ffdbd
Peter Krenesky <peter@osuosl.org>
2010-03-10 16:20:31 -0800

    Slide animation - now uses screen size for determining how far to slide the screen to make it hidden

--- core/templates/screen_animations/slide.js
+++ core/templates/screen_animations/slide.js
@@ -8,6 +8,6 @@ function {{animation.hash}}_in(screen) {
 function {{animation.hash}}_out(screen) {
     id = screen['id']
     duration = 1000;
-    $('#'+id).animate({left:-1400}, duration );
+    $('#'+id).animate({left:-50-screenWidth()}, duration );
     return duration;
 }
