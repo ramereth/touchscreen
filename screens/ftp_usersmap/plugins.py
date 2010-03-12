5bb4ed606f830cbc392281e678732b3d04c657eb
Peter Krenesky <peter@osuosl.org>
2010-03-12 08:36:27 -0800

    FTP Ping Map:    * added config form    * map should now recover from error with ftp location service    * removed unneeded console log

--- screens/ftp_usersmap/plugins.py
+++ screens/ftp_usersmap/plugins.py
@@ -2,12 +2,26 @@ from core.models import Screen, ScreenGeneralSettings
 from django import forms
 
 
+class FTPPingMapSettings(forms.Form):
+    ping_interval = forms.IntegerField(
+            label='Ping Interval',
+            help_text='how often to ping the map in milliseconds',
+            initial=700,
+    )
+    
+    ping_max = forms.IntegerField(
+            label='Ping Max Count',
+            help_text='how many pings to leave on the screen before removing them',
+            initial=100,
+    )
+
+
 class FTP_Users_Map(Screen):    
     template='ftpusersmap.html'
     description='Realtime sample of ftp users'
 
     #optional params
-    config_form=(ScreenGeneralSettings)
+    config_form=(FTPPingMapSettings, ScreenGeneralSettings)
     show='fade'
     hide='fade'
     js_init ='init'
