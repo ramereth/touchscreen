2c7929d4aba95c6e299bbfbf0ea7121942a1206f
Rob McGuire-dale <rob@osuosl.org>
2010-03-12 21:44:48 -0800

    refreshImage "now" command added, along with comment documentation for entire plugin.

--- static/js/jquery.refreshImage.js
+++ static/js/jquery.refreshImage.js
@@ -1,10 +1,79 @@
+/**
+ * jQuery.refreshImage
+ * Copyright (c) 2010 Oregon State Univerisity - info(at)osuosl.org | osuosl.org
+ * licensed under GPLv3.
+ * Date: march 12th 2010
+ *
+ * @projectDescription jQuery extension for automatically refreshing an image.
+ * @author Rob McGuire-Dale (rob(at)osuosl.org)
+ * @version 1.0
+ *
+ * @id jQuery.refreshImage
+ * @id jQuery.fn.refreshImage
+ *
+ * USAGE:
+ * 
+ *  $('.images').refreshImage({interval:<milliseconds>,start:true|false}|"start"|"now"|"stop", ["debug"]);
+ * 
+ *  {interval:<milliseconds>, start:true|false}:
+ *      This is a dictionary of settings specifying the refresh interval and
+ *      whether or not to start the refresh cycle right away. These settings are
+ *      optional. If none are set, defaults will be used. (30 second interval
+ *      and does not start the cycle)
+ *
+ *  "start"
+ *      Starts the refresh cycle, but does NOT refresh immediately.
+ *
+ *  "now"
+ *      Starts the refresh cycle and refreshes immediately.
+ *
+ *  "stop"
+ *      Stops the refresh cycle.
+ *
+ * 
+ * EXAMPLES:
+ *
+ *  $('.images').refreshImage({interval:30000, start:true});
+ *      Sets the refresh interval to 30 seconds, and sarts the refresh cycle,
+ *      but does not refresh immediately. 
+ *
+ *  $('.images').refreshImage("start");
+ *      Starts the refresh cycle, but will not immediately refresh.
+ *
+ *  $('.images').refreshImage("now");
+ *      Starts the refresh cycle, AND refreshes immediately.
+ *
+ *  $('.images').refreshImage("stop");
+ *      Stops the refresh cycle.
+ *
+ * 
+ * RETURNS:
+ * 
+ *  This returns the same matched jQuery objects, for chaining. If there is any
+ *  kind of error, it will return a -1.
+ *
+ *
+ * NOTES:
+ * 
+ *  The plugin loads each image using a different url because it is not possible
+ *  to clear the image from the browser's cache. This will result in increased
+ *  memory usage as time goes on.  If your browser does not properly dispose of
+ *  the cache it will grow indefinitely.  
+ *
+ *  Tested this in firefox.  Firefox3 has a cache size around 350megs. It will
+ *  garbage collect the majority of the cache if it reaches that size.
+ *
+ *  This plugin is a drop-in (almost) rewrite of the refreshImage plugin written
+ *  by Peter Krenesky (kreneskyp(at)gmail.com) in March 2010.
+ */
+
 (function($){
     
     $.fn.extend({
 
         refreshImage: function( arg, debug )
         {            
-            // flag debug mode if it's value is "debug"
+            // flag debug mode if the debug argument's value is "debug"
             debug = debug == "debug";
             
             if(debug) console.info("refreshImage: debug mode on");
@@ -60,9 +129,18 @@
                     
                         if(debug)
                             console.log("refreshImage: 'start' command " +
-                                "entered. Starting refresh cycle now.");
+                                "entered. Starting refresh cycle.");
 
-                        startRefreshing();
+                        startTimer();
+
+                    // if command is "now", start refreshing the image
+                    } else if( arg == "now" ){
+                    
+                        if(debug)
+                            console.log("refreshImage: 'now' command " +
+                                "entered. Refreshing NOW.");
+
+                        refreshNow();
 
                     // if command is "stop", clear the timer to stop the
                     // refresh
@@ -93,22 +171,22 @@
                 if( options.start == true ){ 
                 
                     if(debug)
-                        console.log("refreshImage: option 'start' == true."
-                            +" Starting refresh cycle.");
+                        console.log("refreshImage: option 'start' == true." +
+                            " Starting refresh cycle. Image will refresh in " +
+                            options.interval + " milliseconds."
+                        );
 
-                    startRefreshing();
+                    startTimer();
                 }
                
                 //////////////////
                 // facilitators //
                 //////////////////
 
-                // startRefreshing()
+                // refreshNow()
                 // begin refreshing the image immediately
-                function startRefreshing()
-                {
-                    var options = image.data('refreshImage_options');
-                    
+                function refreshNow()
+                {                    
                     // refresh the image if a src is defined
                     if( image.attr('src') != undefined ){
                         
@@ -128,12 +206,11 @@
                     } else
                         if(debug) console.warn("refreshImage: src undefined.");
 
-                    // set the timout, and save it in the element
-                    var timer =
-                        setTimeout( function(){startRefreshing()}, options.interval);
-                    image.data( 'refreshImage_timer', timer );
+                    // set the timout
+                    startTimer();
                 }
 
+                // stop the refresh cycle by clearing the timer
                 function stopRefreshing()
                 {
                     if(debug)          
@@ -144,6 +221,14 @@
                     if( timer != undefined ) clearTimeout( timer );
                 }
 
+                // set the timeout to start the refresh cycle
+                function startTimer()
+                {
+                    var options = image.data('refreshImage_options');
+                    stopRefreshing(); // clear the old timer
+                    var timer = setTimeout(function(){refreshNow()}, options.interval);
+                    image.data( 'refreshImage_timer', timer );
+                }
             });
         }
     });
