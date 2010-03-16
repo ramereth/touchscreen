f46f3fdafe4c85255f0a36708d93153b9c76d698
Rob McGuire-dale <rob@osuosl.org>
2010-03-15 21:34:00 -0700

    Added new net people screen based on the OSL people screen, and have begun modification.

---
+++ screens/net_people/plugins.py
@@ -0,0 +1,44 @@
+from core.models import Screen, ScreenGeneralSettings
+from django import forms
+
+class NetPeopleSettings(forms.Form):
+    thumbnail_height = forms.IntegerField(
+            label='Thumbnail Image Height',
+            help_text='The height of the thumbnail images',
+            initial=150
+    )
+    
+    scroll_interval = forms.IntegerField(
+        label='Scroll Interval',
+        help_text='Time between scrolling (in milliseconds). Must be at least 50 more than scroll speed.',
+        initial=3000
+    )
+    
+    scroll_speed = forms.IntegerField(
+        label='Scroll Speed',
+        help_text='Time it takes to scroll a single item (in milliseconds). Must be at least 50 less than scroll interval.',
+        initial=1000
+    )
+    
+    auto_rotation_interval = forms.IntegerField(
+        label='Auto Rotation Interval',
+        help_text='Time between people auto rotations (in milliseconds).',
+        initial=10000
+    )
+    
+    rotation_pause_timeout = forms.IntegerField(
+        label='Auto Rotation Pause Timeout',
+        help_text='Time from when a person is explicitly selected to be shown to when auto rotation starts again (in milliseconds).',
+        initial=180000
+    )
+
+class NetPeopleScreen(Screen):
+    template='net_people.html'
+    description="Photos of the employees of Network Services"
+    config_form=(NetPeopleSettings, ScreenGeneralSettings)
+
+    #optional params
+    js_init ='init'
+    js_start='start'
+    js_stop ='stop'
+    js_onWinResize = 'onScreenResize'
